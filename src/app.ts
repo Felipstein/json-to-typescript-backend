5ea3146df205a44147352b4eda52f0b64a857563
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-21 13:04:44 -0300

    ✨ feat: Apply OpenAI to transpile

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,49 @@
 import express from 'express';
+import { Configuration, OpenAIApi } from 'openai';
 import 'express-async-errors';
 import 'dotenv/config';
 
 import { setCors } from './config/cors.config';
+import { env } from './config/env.config';
 
 const app = express();
+
+const configuration = new Configuration({
+  apiKey: env.OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 setCors(app);
 
 app.use(express.json());
 
-app.post('/v1/transpile', (req, res) => {
+app.post('/v1/transpile', async (req, res) => {
+  const { json } = req.body;
 
-  return res.json({ ok: true });
+  const result = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `Convert JSON into TypeScript interfaces: ${json}. Don't send any explanations.`,
+      },
+    ],
+  });
+
+  const choices = result.data.choices;
+
+  if(choices.length === 0) {
+    return res.status(500).json({ errorFeedback: 'Não foi possível transpilar no momento, tente novamente mais tarde.' });
+  }
+
+  const { message } = choices[0];
+
+  if(!message) {
+    return res.status(500).json({ errorFeedback: 'Não foi possível transpilar no momento, tente novamente mais tarde.' });
+  }
+
+  return res.json({ result: message.content });
 });
 
 export { app };
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,7 @@
 export interface EnvConfig {
   PORT: number;
   ORIGIN: string;
+  OPENAI_KEY: string;
 }
 
 export const env = process.env as unknown as EnvConfig;
